2b8fa4c0a6bf853f1f26c4805aa6bb0511cd2abd
GetTheMurzyHere <myshalov.re@students.dvfu.ru>
2018-11-15 17:22:31 +1000

    Homework String Formating

---
+++ Two-dimensional Banana/Homework_Rodya_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Домашка
+{
+    class Program
+    {
+        static string removingNotAnAlphabet(string input)
+        { 
+            Regex format_string = new Regex("[^a-zA-Zа-яА-Я]");
+            return format_string.Replace(input, "");
+        }
+
+        static string reversing(char[] ltrs)
+        {
+            Array.Reverse(ltrs);
+            return new string(ltrs);
+        }
+
+        static string AlphabetSorting(char[] ltrs)
+        {
+            Array.Sort(ltrs);
+            return new string(ltrs);
+        }
+            
+        static void Main(string[] args)
+        {
+            Console.Write("Входная строка: ");
+            string input_string = Console.ReadLine();
+
+            string formated = removingNotAnAlphabet(input_string);
+            Console.WriteLine("Строка, содержащая только буквы: " + formated);
+
+            char[] letters = formated.ToCharArray();
+            
+            Console.WriteLine("Реверсированная строка: " + reversing(letters));
+
+            Console.WriteLine("Строка, отсортированная по алфавиту: " + AlphabetSorting(letters));
+            Console.ReadLine();
+        }
+    }
+}
