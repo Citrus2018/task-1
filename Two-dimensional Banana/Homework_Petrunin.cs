4ff92f0b7bce780b24918043d83483e722ec0888
dwarfmason <DwarfMason@users.noreply.github.com>
2018-11-15 17:48:36 +1000

    add Homework_Petrunin.cs

---
+++ Two-dimensional Banana/Homework_Petrunin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string default_str = Console.ReadLine();
+            string formated = ("[^a-z A-Z а-я А-Я]");
+            default_str = Regex.Replace(default_str, formated, "");
+            char[] new_str = default_str.ToCharArray();
+            Array.Reverse(new_str);
+            Console.WriteLine(new_str);
+            Array.Sort(new_str);
+            Console.WriteLine(new_str);
+            Console.ReadLine();
+            
+        }
+    }
+}
