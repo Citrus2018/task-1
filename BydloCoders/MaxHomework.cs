dcd05232d62a67a886fc4ed84b2ac42b358239c3
MAX <max3jk@gmail.com>
2018-11-15 19:01:44 +1000

    add homework

--- BydloCoders/MaxHomework.cs
+++ BydloCoders/MaxHomework.cs
@@ -7,25 +7,28 @@ namespace Citrus {
             Console.WriteLine("Строка:");
             var s = Console.ReadLine();
 
-            Console.WriteLine("Удалить всё кроме букв:");
-            foreach (var c in s)
-                if (!char.IsLetter(c)) Console.Write(c);
-            Console.WriteLine("\n");
 
+            Console.WriteLine("Удалить всё кроме букв:");
+            var newS = "";
+            foreach (var c in s) if (char.IsLetter(c)) newS += c;
+            Console.WriteLine(newS + "\n");
+            s = newS;
 
 
             Console.WriteLine("Перевернуть строку:");
+            newS = "";
             var chars = s.Reverse();
-            foreach (var c in chars) Console.Write(c);
-            Console.WriteLine("\n");
-
+            foreach (var c in chars) newS += c;
+            Console.WriteLine(newS + "\n");
+            s = newS;
 
 
             Console.WriteLine("Сортировать строку:");
+            newS = "";
             var list = s.ToList();
             list.Sort();
-            foreach (var c in list) Console.Write(c);
-            Console.WriteLine("\n");
+            foreach (var c in list) newS += c;
+            Console.WriteLine(newS + "\n");
 
             Console.ReadLine();
         }
