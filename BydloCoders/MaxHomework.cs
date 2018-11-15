d5df7d62dacc5647adb21cea39c15c33f98959af
MAX <max3jk@gmail.com>
2018-11-15 18:46:39 +1000

    add homework

---
+++ BydloCoders/MaxHomework.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Citrus {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Строка:");
+            var s = Console.ReadLine();
+
+            Console.WriteLine("Удалить всё кроме букв:");
+            foreach (var c in s)
+                if (!char.IsLetter(c)) Console.Write(c);
+            Console.WriteLine("\n");
+
+
+
+            Console.WriteLine("Перевернуть строку:");
+            var chars = s.Reverse();
+            foreach (var c in chars) Console.Write(c);
+            Console.WriteLine("\n");
+
+
+
+            Console.WriteLine("Сортировать строку:");
+            var list = s.ToList();
+            list.Sort();
+            foreach (var c in list) Console.Write(c);
+            Console.WriteLine("\n");
+
+            Console.ReadLine();
+        }
+    }
+}
