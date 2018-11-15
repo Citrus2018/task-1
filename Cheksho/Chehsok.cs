356bbd239df99187e23dd041d047fbff15c33497
KHIPSTERcat <44798549+KHIPSTERcat@users.noreply.github.com>
2018-11-15 16:59:09 +1000

    Add Cheshok team

---
+++ Cheksho/Chehsok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Text.RegularExpressions;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+             s= Console.ReadLine();
+            s = Regex.Replace(s, "[!-@]", "", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[[-`]", "", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[{-Џ]", "", RegexOptions.IgnoreCase);
+            s = Regex.Replace(s, "[ѐ-￿]", "", RegexOptions.IgnoreCase);
+            Console.WriteLine(s);
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);        
+            Console.WriteLine(arr);
+            Array.Sort(arr);            
+            Console.WriteLine(arr);
+            Console.ReadLine();
+        }
+    }
+}
